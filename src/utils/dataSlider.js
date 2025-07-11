35468351a8b5ae0224b4bfa5720a10ff9671822a
Aldana Micaela Filiberto <aldana.filiberto17@gmail.com>
2025-07-11 20:14:28 -0300

    cambios finales

--- src/utils/dataSlider.js
+++ src/utils/dataSlider.js
@@ -3,7 +3,7 @@ export function cargarImg(cant_imagenes_carrusel) {
     const carruselImg = [];
 
     for (let i = 1; i <= cant_imagenes_carrusel; i++) {
-        carruselImg.push(`../public/imgSlider/img${i}.webp`);
+        carruselImg.push(`/imgSlider/img${i}.webp`);
     }
 
     return carruselImg;
@@ -13,7 +13,7 @@ export function cargarImgProductos(cant_imagenes_carrusel) {
     const carruselImg = [];
 
     for (let i = 1; i <= cant_imagenes_carrusel; i++) {
-        carruselImg.push(`../public/pasteleria/torta${i}.jpeg`);
+        carruselImg.push(`/pasteleria/torta${i}.jpeg`);
     }
 
     return carruselImg;
